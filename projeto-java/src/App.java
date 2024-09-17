6ffffa3416ba6e156fb176f0a3cfa9244922a2b8
KGCRM5 <klausgustavo123@gmail.com>
2024-09-17 20:30:10 -0300

    24 número gay

--- projeto-java/src/App.java
+++ projeto-java/src/App.java
@@ -1,5 +1,13 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Iniciando");
+
+        int num = 24;
+        if (num == 24) {
+            System.out.println("gay xd");
+            
+        } else{
+            System.out.println("not gay nice");
+        }
     }
 }
